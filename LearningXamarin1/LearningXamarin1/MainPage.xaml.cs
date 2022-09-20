71f509a19d48765212be294e752b2236fcfdecb7
tchoumi313 <tchouminzikeubd@gmail.com>
2022-09-20 22:25:18 +0100

    DataBinding
    Rotating a label by changing the value of a slider

---
+++ LearningXamarin1/LearningXamarin1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LearningXamarin1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var slider = (Slider)sender;
+            double value = e.NewValue;
+            rotatingLabel.Rotation = value;
+            sliderValue.Text = value.ToString();
+
+
+        }
+    }
+}
