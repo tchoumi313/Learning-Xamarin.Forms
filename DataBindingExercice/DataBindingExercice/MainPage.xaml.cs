e13b1c2edbdc23ca80ad64746449e24eaef56e8d
tchoumi313 <tchouminzikeubd@gmail.com>
2022-09-25 20:36:03 +0100

    DataBinding Exercice Xamarin.Forms
    Binding the change of a  slider and the action on button to a label

---
+++ DataBindingExercice/DataBindingExercice/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataBindingExercice
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<string> Items { get; set; }
+        public List<Color> colors { get; set; } 
+        public int i;
+        public MainPage()
+        {
+            InitializeComponent();
+            i = 0;
+            colors = new List<Color>() { Color.Red, Color.Green,Color.Blue};
+            Items = new List<string>() { "hello","world!", "nothing", "awesone", "donot"};
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {   
+            if(i >= Items.Count)
+            {
+                i = 0;
+            }
+            
+            quoteLabel.Text = Items[i].ToString();
+            i++;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            quoteLabel.TextColor = colors[((int)(e.NewValue))];
+
+        }
+
+        private void sizeSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            sizeLabel.Text = "FontSize : "+ e.NewValue.ToString();
+
+        }
+    }
+}
